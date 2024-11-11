f1295aebb86b7773bc43ef8ce2f46a47d6b91e35
Mohamed Assaf UM <264mdassaf@gmail.com>
2024-11-11 15:48:04 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+# Test log messages
+logging.info("This is an informational message for testing.")
+logging.warning("This is a warning message for testing.")
+logging.error("This is an error message for testing.")
